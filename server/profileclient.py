0501a274b1974fb74f2ea0176c4926a56017e7be
Rohan Garg <rohangarg1998s@gmail.com>
2020-06-09 14:51:51 -0700

    added mem test

--- server/profileclient.py
+++ server/profileclient.py
@@ -1,10 +1,12 @@
 import cProfile, pstats
 import functioncalls
+import tracemalloc
 
 class Profiler:
 
     def __init__(self):
         self.name = "Test"
+
 
     def testProfile(self):
         pr = cProfile.Profile()
@@ -16,6 +18,14 @@ class Profiler:
         ps = pstats.Stats(pr).sort_stats(sortby)
         ps.print_stats()
 
+    def testMemUsage(self):
+        testFunc = functioncalls.FunctionCalls()
+        tracemalloc.start()
+        testFunc.Fibonacci(23)
+        current, peak = tracemalloc.get_traced_memory()
+        print(f"Current memory usage is {current / 10**6}MB; Peak was {peak / 10**6}MB")
+        tracemalloc.stop()
+
 if __name__ == "__main__":
     p = Profiler()
-    p.testProfile()
+    p.testMemUsage()
